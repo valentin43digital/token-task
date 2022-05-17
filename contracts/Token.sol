08b64fecfa1cc2c6f16394c7886d2c346d92ff4e
Valentin Alekseev <asted_asd@mail.ru>
2022-05-18 00:50:27 +0700

    Token contract cleaned

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -3,72 +3,81 @@ pragma solidity ^0.8.0;
 
 contract Token {
     address public owner;
-    string public _name;
-    string public _symbol;
-    uint8 public _decimals;
-    uint256 public _totalSupply;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
 
-    mapping(address =>uint256) private _balances;
-    mapping(address => mapping(address => uint256)) private _allowances;
+    mapping(address =>uint256) private balances;
+    mapping(address => mapping(address => uint256)) private allowances;
 
     modifier ownerOnly() {
         require(msg.sender == owner, "You are not owner");
         _;
     }
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    constructor(string memory newName, string memory newSymbol, uint8 newDecimals, uint256 newTotalSupply) {
-        _name = newName;
-        _symbol = newSymbol;
+    constructor(
+        string memory newName,
+        string memory newSymbol,
+        uint8 newDecimals,
+        uint256 newTotalSupply
+    )
+    {
+        name = newName;
+        symbol = newSymbol;
         owner = msg.sender;
-        _decimals = newDecimals;
-        _totalSupply = newTotalSupply;
-        _balances[owner] = _totalSupply;
+        decimals = newDecimals;
+        totalSupply = newTotalSupply;
+        balances[owner] = totalSupply;
+        emit Transfer(address(0), owner, totalSupply);
     }
 
-    function balanceOf(address _owner) public view returns (uint256){
-        return _balances[_owner];
+    function balanceOf(address account) public view returns (uint256){
+        return balances[account];
     }
 
-    function allowance(address _owner, address _spender) public view returns (uint256){
-        return _allowances[_owner][_spender];
+    function allowance(address owner, address spender) public view returns (uint256){
+        return allowances[owner][spender];
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success){
-        require(_balances[msg.sender] > _value, "Not enough balance");
-        _balances[msg.sender] -= _value;
-        _balances[_to] += _value;
-        emit Transfer(msg.sender, _to, _value);
+    function transfer(address to, uint256 value) public returns (bool success){
+        require(balances[msg.sender] > value, "Not enough balance");
+        balances[msg.sender] -= value;
+        balances[to] += value;
+        emit Transfer(msg.sender, to, value);
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
-        require(_balances[_from] > _value, "Not enough balance");
-        _balances[_from] -= _value;
-        _balances[_to] += _value;
-        emit Transfer(_from, _to, _value);
+    function transferFrom(address from, address to, uint256 value) public returns (bool success){
+        require(balances[from] > value, "Not enough balance");
+        balances[from] -= value;
+        balances[to] += value;
+        emit Transfer(from, to, value);
         return true;
     }
     
-    function approve(address _spender, uint256 _value) public returns (bool success){
-        require(_balances[msg.sender] > _value, "Not enough balance");
-        _allowances[msg.sender][_spender] += _value;
-        emit Approval(msg.sender, _spender, _value);
+    function approve(address spender, uint256 value) public returns (bool success){
+        require(balances[msg.sender] > value, "Not enough balance");
+        allowances[msg.sender][spender] += value;
+        emit Approval(msg.sender, spender, value);
         return true;
     }
 
     function burn(address account, uint256 amount) public ownerOnly {
-        require(_balances[account] > amount, "Not enough balance");
-        _totalSupply -= amount;
-        _balances[account] -= amount;
+        require(account != address(0), "Zero address");
+        require(balances[account] > amount, "Not enough balance");
+        totalSupply -= amount;
+        balances[account] -= amount;
         emit Transfer(account, address(0), amount);
     }
 
     function mint(address account, uint256 amount) public ownerOnly {
-        _totalSupply += amount;
-        _balances[account] += amount;
+        require(account != address(0), "Zero address");
+        totalSupply += amount;
+        balances[account] += amount;
         emit Transfer(address(0), account, amount);
     }
 }
