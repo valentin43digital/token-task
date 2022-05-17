90403a49cd1d1f4095d119d1126c02c12811d5a7
Valentin Alekseev <asted_asd@mail.ru>
2022-05-18 01:09:57 +0700

    Config files changed

--- config.ts
+++ config.ts
@@ -1,8 +1,8 @@
 const configts = {
-    CONTRACT_ADDRESS: "0x691d3E4bDD74fFb40E81cF070d4004Dd1CaCCD9F",
+    CONTRACT_ADDRESS: "0x3f3f02c98Da2487DE0ecaE0d9C9af9dCe8D6d2ce",
     totalSupply: "1000000.0",
-    name: "Token name",
-    symbol: "TKN",
+    name: "Valentin Token",
+    symbol: "VLNT",
     decimals: "18",
     amount1: "100.0",
     amount2: "2000000.0"
