b0dea386948aeabdd06c438f1b011fb4e1a352dc
Valentin Alekseev <asted_asd@mail.ru>
2022-05-18 23:50:31 +0700

    add verify command

--- config.ts
+++ config.ts
@@ -8,3 +8,5 @@ const configts = {
     amount2: "2000000.0"
 }
 export default configts
+
+// npx hardhat verify --network rinkeby 0x3f3f02c98Da2487DE0ecaE0d9C9af9dCe8D6d2ce "Valentin Token" "VLNT" 18 1000000000000000
